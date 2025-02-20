aa2fb2576e8bdcad39be231b4763d2e0eeaca938
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-02-20 14:31:21 +0100

    paid page beginnetje

--- paid.php
+++ paid.php
@@ -1,1 +1,32 @@
 -
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/paid-page.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Kiosk</title>
+
+</head>
+
+<body>
+    <header class="kiosk-header">
+        <div class="logo-container">
+            <img src="assets/img/co_logo.png" alt="Logo" class="logo">
+        </div>
+
+    </header>
+    <div class="thanks-container">
+        Thanks for Ordering!
+    </div>
+
+
+
+</body>
+
+<script src="assets/js/choose-press.js"></script>
+
+</html>
