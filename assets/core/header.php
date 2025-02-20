5af63e26fdba3ee57235a97513690813741256e2
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-02-20 09:27:53 -0800

    started menu

---
+++ assets/core/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <title>Kiosk</title>
+</head>
+<body>
--- menu.php
+++ menu.php
@@ -1,1 +1,17 @@
--
+<?php include_once 'assets/core/header.php'; ?>
+<main>
+    <div id="welcomebar">
+        <img src="assets/img/logo.png" alt="logo" id="logo">
+        <p>WELCOME TO HAPPY HERBIVORE</p>
+    </div>
+    <sidebar>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </sidebar>
+    
