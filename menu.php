9e93b2b564991fda05deb0c42e99b629bd152d53
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-02-20 10:41:13 -0800

    SCROLL

--- menu.php
+++ menu.php
@@ -5,12 +5,37 @@
         <img src="assets/img/logo.png" alt="logo" id="logo">
         <p>WELCOME TO HAPPY HERBIVORE</p>
     </div>
-    <nav id="menu-sidebar">
-        <ul>
-            <li class="menu-category">
-                <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
-                <p class="category-title">BREAKFAST</p>
-            </li>
-        </ul>
-    </nav>
+    <div id="menu-container">
+        <nav id="menu-sidebar">
+            <ul>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+                <li class="menu-category">
+                    <img src="assets/img/breakfast1.webp" alt="menu" class="category-icon">
+                    <p class="category-title">BREAKFAST</p>
+                </li>
+            </ul>
+        </nav>
+        <div id="menu-content">
+
+        </div>
+    </div>
     
