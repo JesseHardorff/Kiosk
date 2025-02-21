c5beedfc0c824985f0b9d5a8fcc6018dd8b5964d
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-02-21 14:01:02 -0800

    did content

--- cart.php
+++ cart.php
@@ -17,5 +17,91 @@
     </div>
     
     <div id="header"><h1>YOUR ORDER</h1><p id="total-price">€8.88</p></div>
+    <div id="content">
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+        <div class="item">
+            <img src="" alt="" class="item-img">
+            <div class="name-price">
+                <h2>Item Name</h2>
+                <p>€8.88</p>
+            </div>
+            <div class="detail-box"></div>
+            <div class="amount-box">
+                <button class="amount-minus">-</button>
+                <p class="amount-text">1</p>
+                <button class="amount-plus">+</button>
+                <button class="amount-clear">X</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
