726b0b81b0b9fd2d25523f0c0184c41d4691982d
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2025-02-21 12:35:21 -0800

    advert & cart working

--- cart.php
+++ cart.php
@@ -1,1 +1,21 @@
--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/cart.css">
+    <title>Kiosk</title>
+</head>
+<body>
+    <div id="advert-cart">
+        <div id="advert"></div>
+        <div id="cart">
+            <img>
+            <p>##</p>
+        </div>
+    </div>
+    
+    <div id="header"><h1>YOUR ORDER</h1><p id="total-price">€8.88</p></div>
+</body>
+</html>
